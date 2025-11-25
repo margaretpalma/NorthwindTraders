e62ca22de16b61694849930b826763a27a3f7811
margaretpalma <mpalma1496@gmail.com>
2025-11-25 15:05:09 -0500

    ic

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+import java.sql.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String c = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root";
+        String password = "yearup";
+        String database = "northwind";
+        String url =
+
+    }
+}
